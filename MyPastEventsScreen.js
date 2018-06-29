af2e1906b46fd80712c256a4f7997d109b6182b0
Ian Phillips <ian>
2018-06-29 13:49:46 -0600

    feedback form

--- MyPastEventsScreen.js
+++ MyPastEventsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Picker, WebView } from 'react-native';
+import { StyleSheet, Text, View, Button, Picker, WebView, FlatList } from 'react-native';
 import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
 
 class MyPastEventsScreen extends React.Component {
@@ -7,6 +7,10 @@ class MyPastEventsScreen extends React.Component {
 		return(
 			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
 				<Text> My Past Events </Text>
+				<FlatList 
+					data={[{key: 'a'}, {key: 'b'},{key: 'c'}, {key: 'd'}, {key: 'e'}]}
+					renderItem={({item}) => <Text>{item.key}</Text>}
+				/>
 			</View>
 		);
 	}
