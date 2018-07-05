c8ef490b8ebfee959f929d33ec3f78dc3d3fd439
rjmorley <ian>
2018-07-05 12:41:47 -0600

    some minor changes and improvements

---
+++ LaunchPage.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Picker, WebView } from 'react-native';
+import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
+
+class LaunchPage extends React.Component {
+	render() {
+		return(
+			<View style={styles.container}>
+				<Text> Launch page </Text>
+        <View style={styles.logocontainer}>
+          <Image source={require('./img/gwln_logo.jpg')} style={styles.logo} />
+        </View>
+			</View>
+		);
+	}
+
+}
+
+export default LaunchPage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    width: null,
+    padding: 20,
+  },
+
+  logocontainer: {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+  },
+
+  logo: {
+    flex: 1,
+    resizeMode: 'contain',
+    width: 250,
+    height: 150,
+  },
+});
