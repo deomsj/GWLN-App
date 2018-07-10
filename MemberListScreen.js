790127bc44031df386c639fd43ed789eed3a1c47
abbirosen <abni1482@colorado.edu>
2018-07-10 09:39:56 -0600

    minor changes

--- MemberListScreen.js
+++ MemberListScreen.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Picker, WebView } from 'react-native';
 import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
+import { SearchBar } from 'react-native-elements';
 
 class MemberListScreen extends React.Component {
+
 	render() {
 		return(
 			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
 				<Text> Member List </Text>
 			</View>
+
 		);
 	}
-	
+
 }
 
 export default MemberListScreen;
