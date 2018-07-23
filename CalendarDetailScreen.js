08fac63ba3b6959b003ed10e73b0914d2e1789f3
rjmorley <ian>
2018-07-23 14:10:59 -0600

    passing date from calendar to details

--- CalendarDetailScreen.js
+++ CalendarDetailScreen.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Picker, WebView } from 'react-native';
 import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
+import EventData from './www_timeline_events.json';
 
 class CalendarDetailScreen extends React.Component {
 
+	constructor() {
+		super();
+		this.state = {
+			data: EventData,
+		}
+	}
 
 	render() {
 		// run query of events on the day that is passed then store the information in an array of objects
 
 		return(
 			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-				<Text> CalendatDetailScreen </Text>
+				<Text> {this.props.navigation.state.params.date.month} </Text>
 			</View>
 		);
 	}
